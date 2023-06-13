3c42a52173ff80a4e1f14d9afd926d62a9c69780
nothingdev <nothingtillsomething@gmail.com>
2023-06-13 12:14:23 +0100

    First Commit

--- next.config.js
+++ next.config.js
@@ -5,6 +5,20 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: config.base_path !== "/" ? config.base_path : "",
   trailingSlash: config.site.trailing_slash,
-};
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+
+}
+
 
 module.exports = nextConfig;
